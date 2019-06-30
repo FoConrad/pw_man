6840ec48b8b6f69bc0e0639ad892e4b964cde5a0
Conrad Christensen <conradbchristensen@gmail.com>
2019-06-30 06:01:19 -0400

    Fixed issue of setting pass twice
    Now set pass can be called multiple times for the same tag without causing problems. If the -o flag is passed to set then it overwrites the old value, else dies with warning message

--- pw_man.sh
+++ pw_man.sh
@@ -52,15 +52,32 @@ usage() {
     echo "Error: $1"
     echo
   fi
-  echo "Usage: pw_man [options] command [args]"
-  echo
-  echo "Options: -i <id> - use identity <id> instead of default"
-  echo "         -h      - print this usage screen"
-  echo
-  echo "Commands: init        - initialize pw_man for id <id>"
-  echo "          [get] <tag> - retrieve password for <tag>"
-  echo "          set <tag>   - set password for <tag>"
-  echo "          chpass      - change protective password for <id>"
+  cat << EOF
+Usage: pw_man [options] command [args]
+
+pw_man is a manager for passwords. When you call init, you set a master
+password for the specified tag. Then other passwords can saved using that
+single master password (also support for multiple master passwords)
+
+Options: -i <id> - use identity <id> instead of default
+         -h      - print this usage screen
+
+Commands: init                  - initialize pw_man for id <id>
+          [get] <tag>           - retrieve password for <tag>
+
+          set [opts] <tag>      - set password for <tag>
+            set opts:  -o   - allow overwriting if password for <tag> exists
+
+          new [opts [len] [tag] - create new password for tag (or prints) of
+                                  length len (or 10). See man pwgen
+            new opts:  -p   - use pwgen to generate with all following flags
+                       -c   - do not use capital letters
+                       -s   - do not use symbols (e.g. #$@* you!)
+                       -n   - do not use numbers
+                       -r   - choose randomly, no category requirements
+
+          chpass                - change protective password for <id>
+EOF
   exit $exit_code
 }
 
@@ -87,10 +104,8 @@ readpass() {
 
 setpass() {
   init_or_die
+  local over_write=0; [ "$1" = "-o" ] && { over_write=1; shift; }
   local tag="$1"; shift
-  echo -n "Password for ${tag}:"
-  read -s tag_password
-  echo
 
   echo -n "Master password:"
   read -s password
@@ -98,12 +113,39 @@ setpass() {
 
   echo "${password}" | \
     gpg "${GPG_ARGS}" --batch --passphrase-fd 0 -d "${CONFIG_FILE}.gpg" > "${CONFIG_FILE}"
+
+
+  # Confirm master pass
   if [ $? -ne 0 ]; then
     rm "${CONFIG_FILE}"
     die "Bad password"
   fi
 
-  echo "${tag}:${tag_password}" >> "${CONFIG_FILE}"
+  # Need to remove unprotected config file no matter what now that we've
+  # unencrypted it
+
+  # Check if password is already present
+  if grep -E "^${tag}:" "${CONFIG_FILE}" &>/dev/null && (( over_write == 0 ))
+  then # Only breaking here due to 80 char limit
+      rm "${CONFIG_FILE}"
+      die "Pass word already set! Use get or pass -o flag to setpass"
+  fi
+
+  echo -n "Password for ${tag}:"
+  read -s tag_password
+  echo
+
+  # We need to 1) remove tag from file if present and 2) add new pass. Wish
+  # there was a construct, e.g. filter file > file
+  local _tmp="$(mktemp)"
+  {
+      grep -vE "^${tag}:" "${CONFIG_FILE}";
+      echo "${tag}:${tag_password}";
+  } > "${_tmp}"
+  cat "${_tmp}" > ${CONFIG_FILE}
+  rm ${_tmp}
+
+
   rm "${CONFIG_FILE}.gpg"
   echo "${password}" | gpg "${GPG_ARGS}" --batch --passphrase-fd 0 --symmetric "${CONFIG_FILE}"
   rm "${CONFIG_FILE}"
@@ -112,29 +154,16 @@ setpass() {
 
 }
 
-remove_config() {
-  local ret=$?
-  rm "${CONFIG_FILE}"
-  if [ "$ret" -ne 0 ]; then
-    return 1
-  else
-    return 0
-  fi
-}
-
 changepass() {
   init_or_die
   # Unencrypt password store
   echo "Enter old password..."
   gpg "${GPG_ARGS}" -d "${CONFIG_FILE}.gpg" > "${CONFIG_FILE}" || die "Bad password"
 
-  # Need to remove unprotected config file no matter what
-  trap remove_config INT
-
   # Reencrypt password store, overwriting old one
   echo "Enter new password..."
-  gpg "${GPG_ARGS}" --symmetric --yes "${CONFIG_FILE}"
-  remove_config || die "Bad password, keep old one for now..."
+  gpg "${GPG_ARGS}" --symmetric --yes "${CONFIG_FILE}" || \
+      die "Bad password, keep old one for now..."
 }
 
 init_or_die() {
@@ -167,7 +196,22 @@ init() {
   rm "${CONFIG_FILE}"
 }
 
+# Essentially all invocations will end here
+remove_config() {
+  local ret=$?
+  rm -f "${CONFIG_FILE}"
+  if [ "$ret" -ne 0 ]; then
+    exit 1
+  else
+    exit 0
+  fi
+}
+
+
 ############ Main logic here ####################
+
+# No matter what happens, remove unencrypted config file
+trap remove_config EXIT
 
 # Parse arguments, i.e. -h and -i <id>
 while getopts "hi:" opt; do
